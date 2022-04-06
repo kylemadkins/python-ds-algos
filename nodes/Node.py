2b150a63938189087733a1d27c2b1bace1e88178
Kyle Adkins <kyle.m.adkins@gmail.com>
2022-04-06 01:21:58 -0500

    Add examples of how to use Node class

--- nodes/Node.py
+++ nodes/Node.py
@@ -11,3 +11,16 @@ class Node:
 
   def set_link_node(self, link_node):
     self.link_node = link_node
+
+yacko = Node("likes to yak")
+wacko = Node("has a penchant for hoarding snacks")
+dot = Node("enjoys spending time in movie lots")
+
+yacko.set_link_node(dot)
+dot.set_link_node(wacko)
+
+dots_data = yacko.get_link_node().get_value()
+wackos_data = dot.get_link_node().get_value()
+
+print(dots_data)
+print(wackos_data)
