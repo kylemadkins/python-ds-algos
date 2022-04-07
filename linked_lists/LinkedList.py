7ef397feaf13cda3fa4ebf01167978878257dbfe
Kyle Adkins <kyle.m.adkins@gmail.com>
2022-04-06 19:03:31 -0500

    Add method to remove a node

--- linked_lists/LinkedList.py
+++ linked_lists/LinkedList.py
@@ -2,20 +2,20 @@ class Node:
   def __init__(self, value, next_node=None):
     self.value = value
     self.next_node = next_node
-    
+
   def get_value(self):
     return self.value
-  
+
   def get_next_node(self):
     return self.next_node
-  
+
   def set_next_node(self, next_node):
     self.next_node = next_node
 
 class LinkedList:
   def __init__(self, value=None):
     self.head_node = Node(value)
-  
+
   def get_head_node(self):
     return self.head_node
 
@@ -23,6 +23,18 @@ class LinkedList:
     new_node = Node(value)
     new_node.set_next_node(self.head_node)
     self.head_node = new_node
+
+  def remove_node(self, value_to_remove):
+    current_node = self.head_node
+    if current_node.get_value() == value_to_remove:
+      self.head_node = self.head_node.get_next_node()
+    else:
+      while current_node != None:
+        next_node = current_node.get_next_node()
+        if next_node.get_value() == value_to_remove:
+          current_node.set_next_node(next_node.get_next_node())
+          return
+        current_node = next_node
 
   def stringify_list(self):
     result = ""
